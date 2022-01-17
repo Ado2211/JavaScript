1a72c72b1538abb73b1b8014134f704c399b01a8
Adnan Mahmić <mahmica@gmail.com>
2022-01-17 20:51:23 +0100

    JS_Popup-next-previous

--- Projekat-31/main.js
+++ Projekat-31/main.js
@@ -10,22 +10,25 @@ function init() {
 function bindGalleryItems() {
     let images = document.querySelectorAll('.gallery a');
 
+    let index = 0;
     for (let image of images) {
         image.addEventListener('click', showImage);
+        image.dataset.index = index;
+        index++;
     }
 }
 
 function showImage(event) {
     event.preventDefault();
-
     let link = event.target.parentElement.href;
+    let index = event.target.parentElement.dataset.index;
 
     let image = document.querySelector('#gallery-preview img')
 
     image.src = link;
 
     let popup = document.getElementById('gallery-preview');
-
+    popup.dataset.currentIndex = index;
     popup.classList.add('visible');
 }
 
@@ -38,8 +41,56 @@ function keyDownHandler(event) {
         case "Escape":
             hidePopup();
             break;
+        case "ArrowRight":
+            showNextImage();
+            break;
+            case "ArrowLeft":
+            showPreviousImage();
+            break;
     }
 }
 
+function showNextImage() {
+    let popup = document.getElementById('gallery-preview');
+    let currentIndex = popup.dataset.currentIndex;
+
+    currentIndex++;
+
+    let images = document.querySelectorAll('.gallery a');
+
+    currentIndex = currentIndex % images.length;
+
+    let link = images[currentIndex].href;
+
+    let image = popup.querySelector('img');
+
+    image.src = link;
+
+    popup.dataset.currentIndex = currentIndex;
+    popup.classList.add('visible');
+
+}
+
+function showPreviousImage() {
+    let popup = document.getElementById('gallery-preview');
+    let currentIndex = popup.dataset.currentIndex;
+
+    let images = document.querySelectorAll('.gallery a');
+    
+    currentIndex--;
+   
+    if ( currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+
+    let link = images[currentIndex].href;
+
+    let image = popup.querySelector('img');
+
+    image.src = link;
+
+    popup.dataset.currentIndex = currentIndex;
+    popup.classList.add('visible');
+}
 
 window.addEventListener('load', init);
