33041bffb9588d52159bef4ea3c69524509f8597
Adnan Mahmić <mahmica@gmail.com>
2022-01-18 01:44:00 +0100

    JS_modal-input

---
+++ Projekat-32/modal.js
@@ -0,0 +1,109 @@
+/*
+        <div class="modal-window">
+            <div class="modal-message">What is your username</div>
+            <div class="modal-content">
+                <div class="modal-row">
+                    <input type="text" id="modal-input" value="adnanma">
+                </div>
+                <div class="modal-row">
+                    <button>
+                        Confirm
+                    </button>
+
+                    <button>
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>*/
+
+
+function createInputModal(message, defaultValue, callbackConfirm, callbackClose) {
+    let pageBlur = document.createElement('div');
+    pageBlur.classList.add('page-blur');
+
+    let modalHolder = document.createElement('div');
+    modalHolder.classList.add('modal-holder');
+    modalHolder.innerHTML = ` <div class="modal-window">
+     <div class="modal-message">${message}</div>
+     <div class="modal-content">
+         <div class="modal-row">
+             <input type="text" id="modal-input" value="${defaultValue}">
+         </div>
+         <div class="modal-row">
+             <button id="modal-confirm-button">
+                 Confirm
+             </button>
+
+             <button id="modal-close-button">
+                 Close
+             </button>
+         </div>
+     </div>`;
+
+    let modalInput = modalHolder.querySelector('#modal-input');
+    let modalConnfirmButton = modalHolder.querySelector('#modal-confirm-button');
+    let modalCloseButton = modalHolder.querySelector('#modal-close-button');
+
+    modalConnfirmButton.addEventListener('click', () => {
+        let value = modalInput.value;
+
+        callbackConfirm(value);
+
+        pageBlur.remove();
+        modalHolder.remove();
+    });
+
+    modalCloseButton.addEventListener('click', () => {
+        callbackClose();
+
+        pageBlur.remove();
+        modalHolder.remove();
+    });
+
+    document.querySelector('body').appendChild(pageBlur);
+    document.querySelector('body').appendChild(modalHolder);
+}
+
+function createYesNoModal (message, callback) {
+    let pageBlur = document.createElement('div');
+    pageBlur.classList.add('page-blur');
+
+    let modalHolder = document.createElement('div');
+    modalHolder.classList.add('modal-holder');
+    modalHolder.innerHTML = ` <div class="modal-window">
+     <div class="modal-message">${ message }</div>
+     <div class="modal-content">
+         <div class="modal-row">
+             <button id="modal-yes-button">
+                 Yes
+             </button>
+
+             <button id="modal-no-button">
+                 No
+             </button>
+         </div>
+     </div>`;
+
+
+    let modalYesButton = modalHolder.querySelector('#modal-yes-button');
+    let modalNoButton = modalHolder.querySelector('#modal-no-button');
+
+    modalYesButton.addEventListener('click', () => {
+        callback(true);
+
+        pageBlur.remove();
+        modalHolder.remove();
+    });
+
+    modalNoButton.addEventListener('click', () => {
+        callback(false);
+
+        pageBlur.remove();
+        modalHolder.remove();
+    });
+
+    document.querySelector('body').appendChild(pageBlur);
+    document.querySelector('body').appendChild(modalHolder);
+}
